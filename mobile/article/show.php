6e535ba09fd983d48c754cfa67f2caaa305b877d
mhj <694538309@qq.com>
2016-04-18 17:56:07 +0800

    小修小改

--- mobile/article/show.php
+++ mobile/article/show.php
@@ -8,12 +8,32 @@ require_once '../../module/article/article.class.php';
 $oArticle = new article(21);
 $isajax = isset($isajax)?true:false;
 if($isajax){  //用户发布、编辑预览
-    $_POST = strip_sql(daddslashes($_POST));
+    $_POST = strip_sql($_POST);
     strip_key($_POST);
+    require_once '../../include/post.func.php';
+    require_once '../../include/module.func.php';
+    $_POST['content'] = stripslashes($_POST['content']);
+
+    $thumbinput = isset($_POST['thumbinput']) && is_array($_POST['thumbinput'])?$_POST['thumbinput']:array();
+    $contentimage = '';
+
+    if($thumbinput){
+        $contentimage .= '<div class="womdnlsandh">';
+        foreach($thumbinput as $k=>$v){
+            if($v && is_image($v)){
+                $contentimage .= dstripslashes('<img src="'.$v.'" style="display:block;margin:0 auto;" />');
+            }
+        }
+        $contentimage .= '</div>';
+    }
+
+    $_POST['content'] = save_local($contentimage.$_POST['content']);
+    $_POST['content'] = clear_link($_POST['content']);
+    $_POST['introduce'] = addslashes(get_intro($_POST['content'], 200));
     $content = $_POST['content'];
     unset($_POST['content']);
     $_POST = dhtmlspecialchars($_POST);
-    $_POST['content'] = addslashes(dsafe($content));
+    $_POST['content'] = dsafe($content);
     $info = $_POST;
     $codetishicode = $info['code'];
     $id = $info['likes'] = $info['hits'] = $info['comments'] = 0;
